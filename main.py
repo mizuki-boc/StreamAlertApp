03f7606624bca5ca2589b9ec6698e7524ba2f661
MizukiKajita <mizuki0842boc@icloud.com>
2020-09-18 15:09:16 +0900

    change port num

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
@@ -7,4 +8,5 @@ def main():
     return "stream alert app!"
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=8888, threaded=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=port, threaded=True)
